8386e81f4e2dc6e2b084d6922d93b960aece932b
AsifShaikh81 <shaikhasif78866@gmail.com>
2025-09-18 00:03:03 +0530

    17/9/25 power of three khud se kiya

--- app13.js
+++ app13.js
@@ -67,3 +67,14 @@ var isPowerOfTwo = function(n) {
 
 
 };
+
+// power of three -> khud se kiya
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if(n==1) return true 
+    else if(n<1 || (n%2 ==0))return false
+    return isPowerOfThree(n/3)
+};
