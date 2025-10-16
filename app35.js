6c9d3ff592315c2092ebefc6e5ea61abc4ec4574
AsifShaikh81 <shaikhasif78866@gmail.com>
2025-10-16 21:20:25 +0530

    1 pr

---
+++ app35.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  let map = {} // creating map
+  // add 's'-string in the map 
+  for(let i=0 ; i<s.length ; i++){
+    // if no char in map add it else inc char count by 1
+    if(!map[s[i]]){
+        map[s[i]]=1
+    }else {
+        map[s[i]]++
+    }
+  }
+
+  for(let i=0; i < t.length ; t++)
+    // if char is not fount or char count is below zero return false,else dec char count
+    if(!map[t[i]] || map[t[i]] <0 ){
+        return false
+    }else {
+        --map[t[i]]
+    }
+  return true
+};
