a3bc88d480e056cfbd4438b171f1d217d4d6a28c
AsifShaikh81 <shaikhasif78866@gmail.com>
2026-02-03 13:44:10 +0530

    3/2

--- Binary Tree/3.js
+++ Binary Tree/3.js
@@ -33,3 +33,33 @@ var levelOrder = function(root) {
     return ans
     
 };
+
+//*Level Order Traversal - recursive app
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    if(!root) return [] // corner case
+    let ans = [] // init ans
+    // level is to track level
+    let traversal = (curr,level) =>{
+    if(!ans[level]) ans[level] = [] // if no ans[level] initialize with empty array []
+    ans[level].push(curr.val)
+    // if curr.left exist than call traversal function with left node and level +1
+    curr.left && traversal(curr.left ,level+1) 
+    curr.right && traversal(curr.right ,level+1)
+    } 
+    traversal(root ,0)
+    return ans
+
+
+};
