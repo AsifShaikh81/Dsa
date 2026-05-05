0466ac25f4267e990fa2fb579f6b73eca0809659
AsifShaikh81 <shaikhasif78866@gmail.com>
2026-05-05 14:46:19 +0530

    5/5

---
+++ DynamicProgramming/2.js
@@ -0,0 +1,15 @@
+// * 70. Climbing Stairs
+// https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/description/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let dp = [ 0,1, 2];
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+   
+};
